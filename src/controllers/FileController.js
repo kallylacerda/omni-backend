bd8e462a09265889c33a766f37764b7ec35315d6
kallyl <kallyl_lacerda@hotmail.com>
2019-04-13 02:21:01 -0300

    Aplication backend

---
+++ src/controllers/FileController.js
@@ -0,0 +1,24 @@
+const File = require('../models/File');
+const Box = require('../models/Box');
+
+class FileController {
+    async store (request, response) {
+        const box = await Box.findById(request.params.id);
+
+        // Criar um arquivo
+        const file = await File.create({
+            title: request.file.originalname,
+            path: request.file.key,
+        });
+
+        box.files.push(file);
+
+        await box.save();
+        
+        request.io.sockets.in(box._id).emit('file', file);
+
+        return response.json(file);
+    }
+}
+
+module.exports = new FileController();
